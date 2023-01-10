f937b49bfb042c3a2eecf85b4f8a07f260352c9b
imarcher <2488747131@qq.com>
2023-01-10 22:15:31 +0800

    修改put为批量

--- jni/leveldb_sax_db.cc
+++ jni/leveldb_sax_db.cc
@@ -79,7 +79,7 @@ JNIEXPORT void JNICALL Java_leveldb_1sax_db_put
     (JNIEnv *env, jobject, jbyteArray leafTimeKey) {
   size_t size = env->GetArrayLength(leafTimeKey);
   LeafTimeKey *key = (LeafTimeKey*)malloc(size);
-  env->GetByteArrayRegion(leafTimeKey, 0, size, (jbyte*)&key);
+  env->GetByteArrayRegion(leafTimeKey, 0, size, (jbyte*)key);
 //  pool->enqueue(std::bind(&leveldb::DB::Put, db, writeOptions, key));
   int num = size / sizeof(LeafTimeKey);
   for(int i=0;i<num;i++) db->Put(writeOptions, key[i]);
